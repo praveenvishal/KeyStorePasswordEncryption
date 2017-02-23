0b3e3d661eb3cbdc2495efd3566b51c3e0621340
Praveen Vishal <praveen.vishal@snapown.com>
2017-02-23 16:10:32 +0530

    Initial commit

---
+++ app/src/main/java/com/example/praveen/passwordencryptionincorrectway/MainActivity.java
@@ -0,0 +1,108 @@
+package com.example.praveen.passwordencryptionincorrectway;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.SignatureException;
+import java.security.UnrecoverableEntryException;
+import java.security.cert.CertificateException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final String SAMPLE_ALIAS = "MYALIAS";
+
+
+
+    @BindView (R.id.encrypt)
+    EditText edTextToEncrypt;
+
+    @BindView (R.id.encrypted)
+    TextView tvEncryptedText;
+
+    @BindView (R.id.decrypted)
+    TextView tvDecryptedText;
+
+    private EnCryptor encryptor;
+    private DeCryptor decryptor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+
+        encryptor = new EnCryptor();
+
+        try {
+            decryptor = new DeCryptor();
+        } catch (CertificateException | NoSuchAlgorithmException | KeyStoreException |
+                IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @OnClick({R.id.encryptButton, R.id.decryptButton})
+    public void onClick(final View view) {
+
+        final int id = view.getId();
+
+        switch (id) {
+            case R.id.encryptButton:
+                encryptText();
+                break;
+            case R.id.decryptButton:
+                decryptText();
+                break;
+        }
+    }
+
+    private void decryptText() {
+        try {
+            tvDecryptedText.setText(decryptor
+                    .decryptData(SAMPLE_ALIAS, encryptor.getEncryption(), encryptor.getIv()));
+        } catch (UnrecoverableEntryException | NoSuchAlgorithmException |
+                KeyStoreException | NoSuchPaddingException | NoSuchProviderException |
+                IOException | InvalidKeyException e) {
+            Log.e(TAG, "decryptData() called with: " + e.getMessage(), e);
+        } catch (IllegalBlockSizeException | BadPaddingException | InvalidAlgorithmParameterException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void encryptText() {
+
+        try {
+            final byte[] encryptedText = encryptor
+                    .encryptText(SAMPLE_ALIAS, edTextToEncrypt.getText().toString());
+            tvEncryptedText.setText(Base64.encodeToString(encryptedText, Base64.DEFAULT));
+        } catch (UnrecoverableEntryException | NoSuchAlgorithmException | NoSuchProviderException |
+                KeyStoreException | IOException | NoSuchPaddingException | InvalidKeyException e) {
+            Log.e(TAG, "onClick() called with: " + e.getMessage(), e);
+        } catch (InvalidAlgorithmParameterException | SignatureException |
+                IllegalBlockSizeException | BadPaddingException e) {
+            e.printStackTrace();
+        }
+    }
+}
